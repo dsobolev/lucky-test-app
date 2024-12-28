e9b724f640b0e294688b1e07e813e452ca0f9dfd
dsobolev <denis.sobolev1@gmail.com>
2024-12-28 01:23:03 +0000

    Controller groups

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\MainController;
 
-Route::get('/register', [PublicController::class, 'index']);
-Route::post('/register', [PublicController::class, 'register'])
-    ->name('register')
-;
-Route::get('/link/{username}/{link}', [PublicController::class, 'showLink'])
-    ->name('link')
-;
+Route::controller(PublicController::class)->group(function () {
+    Route::get('/register', 'index');
+    Route::post('/register', 'register')
+        ->name('register')
+    ;
+    Route::get('/link/{username}/{link}', 'showLink')
+        ->name('link')
+    ;
+});
 
+Route::controller(MainController::class)->group(function () {
+    Route::get('/{link?}', 'index');
+    Route::post('/{link}', 'regenerate');
+    Route::delete('/{link}', 'deactivate');
 
-Route::get('/{link?}', [MainController::class, 'index']);
-Route::post('/{link}', [MainController::class, 'regenerate']);
-Route::delete('/{link}', [MainController::class, 'deactivate']);
+    Route::get('/getlucky/{link}', 'getLucky');
 
-// add link here, apply middleware to all MainCtrl routes
-Route::get('/getlucky/{link}', [MainController::class, 'getLucky']);
-
-Route::get('/history/{link}', [MainController::class, 'history']);
+    Route::get('/history/{link}', 'history');
+});
